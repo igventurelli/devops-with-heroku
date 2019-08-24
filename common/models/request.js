b837d2446f75b6737c5054d1078572c4ef589f68
Igor Venturelli <igventurelli@gmail.com>
2019-08-24 13:13:13 -0300

    Adding CI

--- common/models/request.js
+++ common/models/request.js
@@ -3,33 +3,33 @@
 var app = require('../../server/server');
 
 const schema = {
-  "name": "Request",
-  "options": {
-    "idInjection": false,
-    "pg": {
-      "schema": "gypz",
-      "table": "request"
-    }
-  },
-  "properties": {
-    "id": {
-      "type": "Number",
-      "required": true,
-      "id": 1
+  name: 'Request',
+  options: {
+    idInjection: false,
+    pg: {
+      schema: 'gypz',
+      table: 'request',
     },
-    "customerName": {
-      "type": "String",
-      "required": true,
-      "length": 100
-    }
-  }
+  },
+  properties: {
+    id: {
+      type: 'Number',
+      required: true,
+      id: 1,
+    },
+    customerName: {
+      type: 'String',
+      required: true,
+      length: 100,
+    },
+  },
 };
 
 module.exports = function(Request) {
-  var ds = app.dataSources.pg
+  var ds = app.dataSources.pg;
   ds.createModel(schema.name, schema.properties, schema.options);
-  ds.autoupdate(schema.name, function (err, result) {
-    ds.discoverModelProperties('request', function (err, props) {
+  ds.autoupdate(schema.name, function(err, result) {
+    ds.discoverModelProperties('request', function(err, props) {
     });
   });
 };
